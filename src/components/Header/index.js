a3ca0748c4a36c65383e6cfc480f71d7b932a5e1
Valdir Coxev <valdircoxevdev@gmail.com>
2020-01-26 08:05:29 -0400

    Finalized header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import {Wrapper, Container, Logo, BasketContainer, ItemCount} from './styles';
+import NavigationService from '../../services/navigation';
 
-function Header({navigation, cartSize}) {
-  console.tron.log(navigation);
-  return (
-    <Wrapper>
-      <Container>
-        <Logo onPress={() => navigation.navigate('Cart')} />
-        <BasketContainer onPress={() => navigation.navigate('Cart')}>
-          <Icon name="shopping-basket" color="#fff" size={24} />
-          <ItemCount>{cartSize || 0}</ItemCount>
-        </BasketContainer>
-      </Container>
-    </Wrapper>
-  );
+import {Container, Logo, LogoImage, BasketContainer, ItemCount} from './styles';
+
+function Header({cartSize}) {
+    return (
+        <Container>
+            <Logo onPress={() => NavigationService.navigate('Main')}>
+                <LogoImage />
+            </Logo>
+            <BasketContainer onPress={() => NavigationService.navigate('Cart')}>
+                <Icon name="shopping-basket" color="#FFF" size={24} />
+                <ItemCount>{cartSize || 0}</ItemCount>
+            </BasketContainer>
+        </Container>
+    );
 }
 
-const mapStateToProps = state => ({
-  cartSize: state.cart.length,
-});
-
-export default connect(mapStateToProps, null)(Header);
+export default connect(
+    state => ({
+        cartSize: state.cart.length,
+    }),
+    null
+)(Header);
